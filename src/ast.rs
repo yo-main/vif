6559c115ab946c52282e491cb4fc31a19e25ca0c
Yomain <arnal.romain@gmail.com>
2023-10-08 17:53:23 +0200

    feat: start defining ast

---
+++ src/ast.rs
@@ -0,0 +1,34 @@
+use crate::tokens::Token;
+
+pub enum Operator {
+    value(Token),
+}
+
+pub enum Literal {
+    value(Token),
+}
+
+pub enum Binary {
+    Left(Box<Expr>),
+    Operator(Token),
+    Right(Box<Expr>),
+}
+
+pub enum Unary {
+    Operator(Token),
+    Right(Box<Expr>),
+}
+
+pub enum Grouping {
+    Left(Token),
+    Expr(Box<Expr>),
+    Right(Token),
+}
+
+pub enum Expr {
+    Operator(Operator),
+    Binary(Binary),
+    Unary(Unary),
+    Grouping(Grouping),
+    Literal(Literal),
+}
