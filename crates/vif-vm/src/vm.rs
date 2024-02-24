e6da24b70c92cbc2c24cf3b5540bcb7610c8e597
Yomain <arnal.romain@gmail.com>
2024-02-24 04:17:22 +0100

    clean: comment some vm code that seems useless
    But it leaves me perplex so I'm not deleting it.
    I feel like it may have its use, I just haven't
    been able to prove it by making the program crash

--- crates/vif-vm/src/vm.rs
+++ crates/vif-vm/src/vm.rs
@@ -85,23 +85,28 @@ where
         let result = self.stack.pop();
 
         if self.previous_frames.len() > 0 {
-            match result {
-                // function hasn't been called yet, don't get rid of its variable
-                // it's a hack that won't work all the time (especially after we add classes)
-                // TODO: store inherited variables on the function itself, not the stack
-                //
-                // careful about
-                //
-                // def coucou():
-                //     var x = 1
-                //     x = 2
-                //     def closure():
-                //         x = 1
-                //
-                // when x=2, we need to know it's a inherited variable
-                StackValue::Function(_) => (),
-                _ => self.stack.truncate(self.frame.get_position()),
-            }
+            //// TODO: the code seems working without that piece of code. To be deleted ?
+            //
+            //
+            // match result {
+            // function hasn't been called yet, don't get rid of its variable
+            // it's a hack that won't work all the time (especially after we add classes)
+            // TODO: store inherited variables on the function itself, not the stack
+            //
+            // careful about
+            //
+            // def coucou():
+            //     var x = 1
+            //     x = 2
+            //     def closure():
+            //         x = 1
+            //
+            // when x=2, we need to know it's a inherited variable
+            // StackValue::Function(_) => (),
+            // _ => self.stack.truncate(self.frame.get_position()),
+            // }
+
+            self.stack.truncate(self.frame.get_position());
             self.frame = self.previous_frames.pop().unwrap();
             self.stack.push(result);
         }
